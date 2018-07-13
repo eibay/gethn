bf4b9967e59d659eebed2e0d3f01c652e2346680
Elmer Ibayan <eibayan@gmail.com>
2018-07-14 07:31:10 +1000

    change hourly fetch to daily

--- config/initializers/aggregator.rb
+++ config/initializers/aggregator.rb
@@ -29,10 +29,10 @@ def fetchData()
   end
 end
 
-# Force to run initially to ensure there's data to display
+#Force to run initially to ensure there's data to display
 fetchData
 
 scheduler = Rufus::Scheduler.singleton
-hourly_fetch = scheduler.every '60m' do
+hourly_fetch = scheduler.every '1440m' do
   fetchData
 end
